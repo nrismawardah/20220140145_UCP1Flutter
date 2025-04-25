d6b9dc2b791f30f169697593cae9b152adeb8e6b
nrismawardah <nrismawardah@gmail.com>
2025-04-25 09:32:17 +0700

    Membuat controller untuk input email dan password pada login page

--- lib/ucp/login.dart
+++ lib/ucp/login.dart
@@ -8,6 +8,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
