8052664b3d7d728a09496190a9ac7967b6c834fd
nrismawardah <nrismawardah@gmail.com>
2025-04-25 09:32:56 +0700

    Membuat initialize state login page

--- lib/ucp/login.dart
+++ lib/ucp/login.dart
@@ -10,7 +10,12 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
