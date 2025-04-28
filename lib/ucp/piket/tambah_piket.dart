941fc6a6d375d780fc8b2265bf9e4af7b1abf547
nrismawardah <nrismawardah@gmail.com>
2025-04-29 01:11:19 +0700

    Membuat controller untuk input nama dan tugas serta tanggal dan ldaftar tugas

--- lib/ucp/piket/tambah_piket.dart
+++ lib/ucp/piket/tambah_piket.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 class TambahPiketPage extends StatefulWidget {
   final String email;
   const TambahPiketPage({Key? key, required this.email}) : super(key: key);
-  
+
   @override
   State<TambahPiketPage> createState() => _TambahPiketPageState();
 }
 
 class _TambahPiketPageState extends State<TambahPiketPage> {
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _tugasController = TextEditingController();
+  DateTime? _selectedDate;
+  List<String> _daftarTugas = [];
+  
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
