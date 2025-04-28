7a0591a11a89224d38be3017baf451cc7ff14e3b
nrismawardah <nrismawardah@gmail.com>
2025-04-29 01:05:38 +0700

    Membuat stateful widget untuk tambah piket page

---
+++ lib/ucp/piket/tambah_piket.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TambahPiketPage extends StatefulWidget {
+  const TambahPiketPage({super.key});
+
+  @override
+  State<TambahPiketPage> createState() => _TambahPiketPageState();
+}
+
+class _TambahPiketPageState extends State<TambahPiketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
