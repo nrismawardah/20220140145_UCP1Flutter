37bef9e1a65eddcb2b4127c5af16bd1a16bb4c0d
nrismawardah <nrismawardah@gmail.com>
2025-04-29 02:33:29 +0700

    Membuat stateless widget untuk detail piket page

---
+++ lib/ucp/piket/detail_piket.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailPiketPage extends StatelessWidget {
+  const DetailPiketPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
