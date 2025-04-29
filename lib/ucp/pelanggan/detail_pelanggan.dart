be56944f04a15adfb88a0189b1c3caa20462352e
nrismawardah <nrismawardah@gmail.com>
2025-04-29 17:31:12 +0700

    Menambah variabel nama, email, dll

--- lib/ucp/pelanggan/detail_pelanggan.dart
+++ lib/ucp/pelanggan/detail_pelanggan.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
 class DetailPelangganPage extends StatelessWidget {
-  const DetailPelangganPage({super.key});
+  final String nama;
+  final String email;
+  final String noHp;
+  final String alamat;
+  final String provinsi;
+  final String kodePos;
+
+  const DetailPelangganPage({
+    super.key,
+    required this.nama,
+    required this.email,
+    required this.noHp,
+    required this.alamat,
+    required this.provinsi,
+    required this.kodePos,
+  });
 
   @override
   Widget build(BuildContext context) {
