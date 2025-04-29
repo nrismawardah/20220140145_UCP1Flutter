bd4fdedffa94af46779e132c6d0b1a5fe5d05fd6
nrismawardah <nrismawardah@gmail.com>
2025-04-29 18:21:53 +0700

    Membuat stateful widget untuk tambah barang page

---
+++ lib/ucp/barang/tambah_barang.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TambahBarangPage extends StatefulWidget {
+  const TambahBarangPage({super.key});
+
+  @override
+  State<TambahBarangPage> createState() => _TambahBarangPageState();
+}
+
+class _TambahBarangPageState extends State<TambahBarangPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
