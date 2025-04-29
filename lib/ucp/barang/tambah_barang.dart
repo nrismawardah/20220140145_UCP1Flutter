2dd470cc495f9dd59ffd9c81628f1a80983d8259
nrismawardah <nrismawardah@gmail.com>
2025-04-29 18:36:51 +0700

    Menambah controller, list, dll

--- lib/ucp/barang/tambah_barang.dart
+++ lib/ucp/barang/tambah_barang.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
 class TambahBarangPage extends StatefulWidget {
-  const TambahBarangPage({super.key});
+  const TambahBarangPage({Key? key}) : super(key: key);
 
   @override
   State<TambahBarangPage> createState() => _TambahBarangPageState();
 }
 
 class _TambahBarangPageState extends State<TambahBarangPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _jumlahController = TextEditingController();
+
+  DateTime? _selectedDate;
+  String? _selectedJenisTransaksi;
+  String? _selectedJenisBarang;
+  int _hargaSatuan = 0;
+
+  final _jenisTransaksiList = ['Barang Masuk', 'Barang Keluar'];
+  final Map<String, int> _barangHargaMap = {
+    'Carrier': 540000,
+    'Tenda': 150000,
+    'Sleeping Bag': 50000,
+    'Sepatu': 70000,
+  };
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
