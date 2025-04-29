7ba81a2bbfae8ab72f21b921d7ce9e357f38f791
nrismawardah <nrismawardah@gmail.com>
2025-04-29 15:59:57 +0700

    Membuat controller untuk input data pelanggan

--- lib/ucp/pelanggan/tambah_pelanggan.dart
+++ lib/ucp/pelanggan/tambah_pelanggan.dart
@@ -8,6 +8,13 @@ class TambahPelangganPage extends StatefulWidget {
 }
 
 class _TambahPelangganPageState extends State<TambahPelangganPage> {
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _noHpController = TextEditingController();
+  final TextEditingController _alamatController = TextEditingController();
+  final TextEditingController _provinsiController = TextEditingController();
+  final TextEditingController _kodePosController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
