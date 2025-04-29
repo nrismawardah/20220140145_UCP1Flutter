5de1ddfe039abb46cc63bf4b188992aa5a50e1f7
nrismawardah <nrismawardah@gmail.com>
2025-04-29 14:20:20 +0700

    Membuat stateful widget untuk tambah pelanggan page

---
+++ lib/ucp/pelanggan/tambah_pelanggan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TambahPelangganPage extends StatefulWidget {
+  const TambahPelangganPage({super.key});
+
+  @override
+  State<TambahPelangganPage> createState() => _TambahPelangganPageState();
+}
+
+class _TambahPelangganPageState extends State<TambahPelangganPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
